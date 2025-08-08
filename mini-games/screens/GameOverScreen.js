5b44aa27ca7ff53b36efe7c13ed0e4af2e464b75
tiedsandi <fachransandi@gmail.com>
2025-08-08 21:12:30 +0700

    02. adding game over screen

--- mini-games/screens/GameOverScreen.js
+++ mini-games/screens/GameOverScreen.js
@@ -1,5 +1,56 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
 
 export default function GameOverScreen() {
-  return <Text>Game is Over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    borderRadius: 200,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primar800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
+  },
+});
